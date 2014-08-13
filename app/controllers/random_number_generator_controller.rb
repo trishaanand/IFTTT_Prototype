fa2423566d456c37221d74b867f7486772f7a1a7
Trisha <trisha1990@gmail.com>
2014-08-14 04:42:55 +0530

    rng no authentication required

--- app/controllers/random_number_generator_controller.rb
+++ app/controllers/random_number_generator_controller.rb
@@ -1,4 +1,5 @@
 class RandomNumberGeneratorController < ApplicationController
+	skip_before_filter :authenticate_user!
 	def generate
   	# @num = rand(0.000...1.000)
   	@var = RandomNumberGenerator.new
