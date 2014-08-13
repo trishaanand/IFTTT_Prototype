ed031730f6e95f7e011ea15704804d33ec265b71
Trisha <trisha1990@gmail.com>
2014-08-14 03:15:03 +0530

    compile error fix attempt 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
   Rails.application.routes.draw do
   
-  root :to => 'tuples#index', :as => root 
+  get '/' :to => 'tuples#index'#, :as => root 
 
   devise_for :users do
     get 'logout' => 'devise/sessions#destroy'
